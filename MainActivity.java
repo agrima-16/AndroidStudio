5f49b8ac25d8380700f4b4ba79a5959d0835bd92
agrima-16 <51071442+agrima-16@users.noreply.github.com>
2019-06-07 01:30:46 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,7 @@
-package com.example.agrima.calling;
+package com.example.agrima.dialing;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,46 +9,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import static android.support.v4.content.ContextCompat.createDeviceProtectedStorageContext;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     EditText txt;
-    Button calling;
+    Button dialing;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt=(EditText)findViewById(R.id.no);
-        calling=(Button)findViewById(R.id.dial);
+        dialing=(Button)findViewById(R.id.dialing);
 
-        calling.setOnClickListener(new View.OnClickListener() {
+        dialing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String txt1=txt.getText().toString();
                 if(TextUtils.isEmpty(txt1))
                 {
                     txt.setError("Enter no");
+
                 }
                 else
                 {
-                   if(ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.CALL_PHONE)!=PackageManager.PERMISSION_GRANTED)
-                    {
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.CALL_PHONE}, 0);
-
-
-                    }
-                    else
-                    {
-                        Intent i= new Intent(Intent.ACTION_CALL, Uri.parse("tell:"+txt1)) ;
-                        startActivity(i);
-                    }
-
-
-
-
+                    Intent i=new Intent(Intent.ACTION_DIAL,
+                            Uri.parse("tel:"+txt1));
+                    startActivity(i);
                 }
             }
         });
